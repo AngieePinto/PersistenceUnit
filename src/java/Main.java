fc23999905daf43009939b15eae11198de4f926f
Johanna Guevara <Johanna Guevara@172.18.1.31>
2014-09-04 21:04:57 -0500

    040914

--- src/java/Main.java
+++ src/java/Main.java
@@ -6,6 +6,7 @@ import javax.persistence.Persistence;
 import org.eclipse.persistence.config.ParserType;
 
 /*
+ * 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
